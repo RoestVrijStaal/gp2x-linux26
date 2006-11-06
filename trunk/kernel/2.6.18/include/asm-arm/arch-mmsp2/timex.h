51373207c5c2941065def59f2e1281db8bab33ab
jorgeluis.zapata <jorgeluis.zapata@bb4e0d89-4221-0410-a92a-1118294635fc>
2006-11-06 22:48:39 +0000

    + MMSP2 Kconfig + kernel Makefile + MMSP2 headers

---
+++ trunk/kernel/2.6.18/include/asm-arm/arch-mmsp2/timex.h
@@ -0,0 +1,7 @@
+#ifndef TIMEX_H_
+#define TIMEX_H_
+
+/* 32-bit timer that is clocked by the 7.3728MHz oscillator */
+#define CLOCK_TICK_RATE         7372800
+
+#endif /*TIMEX_H_*/
---
+++ trunk/kernel/2.6.18/include/asm-arm/arch-mmsp2/vmalloc.h
@@ -0,0 +1,6 @@
+#ifndef VMALLOC_H_
+#define VMALLOC_H_
+
+#define VMALLOC_END       (PAGE_OFFSET + 0x10000000)
+
+#endif /*VMALLOC_H_*/
