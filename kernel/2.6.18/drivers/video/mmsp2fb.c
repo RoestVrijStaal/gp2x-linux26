eae09ae68fea1bda3791175764b0be937d30e809
jorgeluis.zapata <jorgeluis.zapata@bb4e0d89-4221-0410-a92a-1118294635fc>
2006-12-05 17:07:31 +0000

    + Fb work

--- kernel/2.6.18/drivers/video/mmsp2fb.c
+++ kernel/2.6.18/drivers/video/mmsp2fb.c
@@ -8,7 +8,19 @@
 #define DRIVER_NAME "mmsp2_fb"
 #define DRIVER_VERSION "0.1"
 
+struct mmsp2_fb_info
+{
+	
+};
 
+
+static int __init mmsp2_fb_map_vmem(struct fb_info *info)
+{
+	struct mmsp2_fb_info *mi = info->par;
+
+	//return mi->map_cpu ? 0 : -ENOMEM;
+	return -ENOMEM;
+}
 /* ==== FB API ==== */
 static int
 mmsp2_fb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
@@ -38,12 +50,12 @@ static int mmsp2_fb_blank(int blank, struct fb_info *info)
 static struct fb_ops mmsp2_fb_ops = {
 	.owner		= THIS_MODULE,
 	.fb_check_var	= mmsp2_fb_check_var,
-	.fb_set_par	= mmsp2_fb_set_par,
+	.fb_set_par		= mmsp2_fb_set_par,
 	.fb_setcolreg	= mmsp2_fb_setcolreg,
 	.fb_fillrect	= cfb_fillrect,
 	.fb_copyarea	= cfb_copyarea,
 	.fb_imageblit	= cfb_imageblit,
-	.fb_blank	= mmsp2_fb_blank,
+	.fb_blank		= mmsp2_fb_blank,
 };
 /* ==== IRQ handling ==== */
 /* just clear the interrupt let the tasklet handle the rest */
@@ -57,16 +69,25 @@ static irqreturn_t mmsp2_fb_irq(int irq, void *devid, struct pt_regs *regs)
 /* ==== platform device API ==== */
 static int mmsp2_fb_probe(struct platform_device *pdev)
 {
+	struct fb_info *fbi;
+	struct mmsp2_fb_info *mi;
 	struct mmsp2_lcd_platform_data *pdata;
 	int ret = 0;
 	
-	/* get the platform data */
 	pdata = pdev->dev.platform_data;
 	if(!pdata)
 	{
 		ret = -EINVAL;
 		goto pdata;	
 	}
+	fbi = framebuffer_alloc(sizeof(struct mmsp2_fb_info), &pdev->dev);
+	if(!fbi)
+		return -ENOMEM;
+
+	mi = fbi->par;
+	
+	platform_set_drvdata(pdev, mi);
+	
 	/* request the irq */
 	/* request the memory region for video memory */
 	/* initialize the lcd */
