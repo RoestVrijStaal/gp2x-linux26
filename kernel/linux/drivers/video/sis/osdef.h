06f7efd152a2cd9e264338ac60a6f3d1a8c24e47
jorgeluis.zapata <jorgeluis.zapata@bb4e0d89-4221-0410-a92a-1118294635fc>
2008-02-07 23:28:22 +0000

    2.6.24

--- kernel/linux/drivers/video/sis/osdef.h
+++ kernel/linux/drivers/video/sis/osdef.h
@@ -58,9 +58,6 @@
 #define SIS_LINUX_KERNEL		/* Linux kernel framebuffer */
 #undef  SIS_XORG_XF86			/* XFree86/X.org */
 
-#undef SIS_LINUX_KERNEL_24
-#undef SIS_LINUX_KERNEL_26
-
 #ifdef OutPortByte
 #undef OutPortByte
 #endif
@@ -98,12 +95,6 @@
 
 #ifdef CONFIG_FB_SIS_315
 #define SIS315H
-#endif
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,5,0)
-#define SIS_LINUX_KERNEL_26
-#else
-#define SIS_LINUX_KERNEL_24
 #endif
 
 #if !defined(SIS300) && !defined(SIS315H)
