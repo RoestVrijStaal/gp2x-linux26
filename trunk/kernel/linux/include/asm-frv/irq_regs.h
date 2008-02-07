93e1dc61ebcd129672ebf147d150e0e0a0b382c9
jorgeluis.zapata <jorgeluis.zapata@bb4e0d89-4221-0410-a92a-1118294635fc>
2008-02-07 18:31:50 +0000

    2.6.24

--- trunk/kernel/linux/include/asm-frv/irq_regs.h
+++ trunk/kernel/linux/include/asm-frv/irq_regs.h
@@ -1,4 +1,4 @@
-/* irq.h: FRV IRQ definitions
+/* FRV per-CPU frame pointer holder
  *
  * Copyright (C) 2006 Red Hat, Inc. All Rights Reserved.
  * Written by David Howells (dhowells@redhat.com)
@@ -9,22 +9,19 @@
  * 2 of the License, or (at your option) any later version.
  */
 
-#ifndef _ASM_IRQ_H_
-#define _ASM_IRQ_H_
+#ifndef _ASM_IRQ_REGS_H
+#define _ASM_IRQ_REGS_H
 
-#define NR_IRQS				48
-#define IRQ_BASE_CPU			(0 * 16)
-#define IRQ_BASE_FPGA			(1 * 16)
-#define IRQ_BASE_MB93493		(2 * 16)
-
-/* probe returns a 32-bit IRQ mask:-/ */
-#define MIN_PROBE_IRQ			(NR_IRQS - 32)
+/*
+ * Per-cpu current frame pointer - the location of the last exception frame on
+ * the stack
+ * - on FRV, GR28 is dedicated to keeping a pointer to the current exception
+ *   frame
+ */
+#define ARCH_HAS_OWN_IRQ_REGS
 
 #ifndef __ASSEMBLY__
-static inline int irq_canonicalize(int irq)
-{
-	return irq;
-}
+#define get_irq_regs() (__frame)
 #endif
 
-#endif /* _ASM_IRQ_H_ */
+#endif /* _ASM_IRQ_REGS_H */
